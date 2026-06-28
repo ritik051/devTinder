dadff75b248ca729749cfa9e95e20dffc4f44dc3
ritik051 <strangerforall3@gmail.com>
2026-06-28 18:03:59 +0530

    Move MongoDB credentials to environment variables

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
+require("dotenv").config();
+
 const express =  require('express');
-const connectDb = require('../config/database');
+const connectDb = require('./config/database');
 const User =  require('./models/user');
 const app = express();
 
@@ -15,7 +17,40 @@ app.post('/signup', async (req,res) => {
         res.status(500).send("Error creating user");
     }
         
-})
+});
+
+app.get('/users', async (req,res) => {
+    try{
+        // const users = await User.find({email: req.body.email});
+        // if(users.length === 0) {
+        //     return res.status(404).send("No users found");
+        // }
+        const users = await User.findOne({email: req.body.email});
+         if(!users) {
+            return res.status(404).send("User not found");
+        }
+        else{
+            res.send(users);
+        }
+    }
+    catch(err) {
+        res.status(500).send("something went wrong")
+    }
+});
+
+app.get('/feed', async (req,res) => {
+    try{
+        const users = await User.find({});
+        if(users.length === 0) {
+            return res.status(404).send("No users found");
+        }else{
+            res.send(users);
+        }
+    }
+    catch(err) {
+        res.status(500).send("something went wrong")
+    }
+});
 
 connectDb().then(() => {
     console.log("Database connected successfully");
